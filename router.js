ceea350a5cf8f12bcb2d02addccaeaecbf46a19b
taeyoung <xoxodudwkd@gmail.com>
2019-09-05 17:57:50 +0900

    seperate url path

--- router.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-export const userRouter = express.Router();
-
-const handlePassword = (req, res) => res.send('user password');
-
-userRouter.get('/', (req, res) => res.send('user index'));
-userRouter.get('/edit', (req, res) => res.send('user edit'));
-userRouter.get('/password', handlePassword);
