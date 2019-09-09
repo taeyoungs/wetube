e034c14775a3194b79e6b0c5726d899f3c56fbbf
taeyoung <xoxodudwkd@gmail.com>
2019-09-09 15:48:21 +0900

    Home Controller with mixins, fake data ...

---
+++ db.js
@@ -0,0 +1,54 @@
+export const videos = [
+  {
+    id: 582932,
+    title: 'Video Sample',
+    description: 'Youtube Clone Coding',
+    views: 24,
+    videoFile:
+      'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
+    creator: {
+      id: 121212,
+      name: 'Youngs',
+      email: 'youngs@nas.com',
+    },
+  },
+  {
+    id: 294856,
+    title: 'Video Wow',
+    description: 'Youtube Clone Coding',
+    views: 24,
+    videoFile:
+      'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
+    creator: {
+      id: 121212,
+      name: 'Youngs',
+      email: 'youngs@nas.com',
+    },
+  },
+  {
+    id: 125394,
+    title: 'Video Awesome',
+    description: 'Youtube Clone Coding',
+    views: 24,
+    videoFile:
+      'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
+    creator: {
+      id: 121212,
+      name: 'Youngs',
+      email: 'youngs@nas.com',
+    },
+  },
+  {
+    id: 106834,
+    title: 'Video Perfect',
+    description: 'Youtube Clone Coding',
+    views: 24,
+    videoFile:
+      'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
+    creator: {
+      id: 121212,
+      name: 'Youngs',
+      email: 'youngs@nas.com',
+    },
+  },
+];
